b3ee0d08c3a75ac02532e2356cdfadbe64df5649
Cameron-Van-Dyke <cjv8@students.ptcollege.edu>
2023-02-27 10:55:41 -0500

    delete

--- BreadCrumbsApp/BreadCrumbs/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BreadCrumbs;
-
-public partial class AppShell : Shell
-{
-	public AppShell()
-	{
-		InitializeComponent();
-	}
-}
--- BreadCrumbsApp/BreadCrumbs/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace BreadCrumbs;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
